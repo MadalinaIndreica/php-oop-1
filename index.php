1f160949b909af6e8890cc7ad40a762d88100a6a
MadalinaIndreica <madalinaindreicamaddy4@gmail.com>
2024-01-08 15:04:24 +0100

    add: first part without bonus

--- index.php
+++ index.php
@@ -1,3 +1,36 @@
+<?php
+    class Movie {
+        public $name;
+        public $year;
+        public $type;
+
+        function __construct($_name, $_year, $_type)
+        {
+            $this->name = $_name; 
+            $this->year = $_year; 
+            $this->type = $_type; 
+
+        }
+        function printFullTitle() {
+            return $this->name . ' ' . $this->year . ' ' . $this->type . ' ';
+        }
+    }
+
+    $firstMovie = new Movie ('nowhere','2023','thriller');
+    echo $firstMovie->printFullTitle();
+    $secondMovie = new Movie ('le pagine della nostra vita', '2004','Romantico');
+    echo $secondMovie->printFullTitle();
+    $thirdMovie = new Movie ('Hich', '2005','commedia');
+    echo $thirdMovie->printFullTitle();
+    //var_dump($firstMovie,$secondMovie,$thirdMovie);
+
+    
+
+
+
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
